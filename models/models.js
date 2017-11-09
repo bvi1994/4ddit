dd5685a903faba2465c7f3d2cb4e9365e90731a2
bvi1994 <bvi1@ucsc.edu>
2017-11-09 15:36:19 -0800

    Got part 3 to work with the require schema that has been approved.

---
+++ models/models.js
@@ -0,0 +1,99 @@
+"use strict";
+
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize(process.env.DATABASE_NAME, 'postgres', process.env.DATABASE_PASSWORD, {
+    dialect: 'postgres'
+});
+
+sequelize
+.authenticate()
+.then(() => {
+    console.log('Connection has been established successfully.');
+})
+.catch(err => {
+    console.error('Unable to connect to the database:', err);
+});
+
+// MODELS GO HERE
+
+var User = sequelize.define('user', {
+  // Rememner that the user ID is generated automatically
+    username: {
+        type: Sequelize.STRING,
+        allownull: false,
+        unique: true,
+    },
+    password: {
+        type: Sequelize.STRING,
+        allownull: false,
+    },
+    commentKarma: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0,
+    },
+    postKarma: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0,
+    },
+});
+
+var Thread = sequelize.define('thread', {
+  // The author key would be from the username as a foreign key
+  // Remember that the thread ID is generated automatically
+    author: {
+        type: Sequelize.STRING,
+        allownull: false,
+        // foreignKeyConstraint: true
+    },
+    content: {
+        type: Sequelize.STRING,
+        allownull: false,
+    },
+    postKarma: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0,
+    },
+});
+
+var Comment = sequelize.define('comment', {
+  // The author key would be from the username as a foreign key
+  // Remember that the comment ID is generated automatically
+    author: {
+        type: Sequelize.STRING,
+        allownull: false,
+        // foreignKeyConstraint: true,
+    },
+    content: {
+        type: Sequelize.STRING,
+        allownull: false,
+    },
+    commentKarma: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0,
+    },
+    // date: {
+    //     type: Sequelize.DATE,
+    //     defaultValue: Sequelize.NOW
+    // },
+  // The thread ID field is the comment belongs to in the thread
+  // We will get that from the thread ID schema as shown above
+    threadID: {
+        type: Sequelize.INTEGER,
+        allownull: false,
+        // foreignKeyConstraint: true,
+    },
+  // This is a reply to a comment. Basically we are having the subcomment
+  // be a child of the comment that we are replying to. Note that if
+  // it is null, this implies that the comment is the starting node.
+    parentID: {
+        type: Sequelize.INTEGER,
+    }
+});
+
+module.exports = {
+    User,
+    Thread,
+    Comment,
+    sequelize,
+    // EXPORT models HERE
+};
--- server.js
+++ server.js
@@ -3,8 +3,59 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const api = require('./backend/routes');
+const bodyParser = require('body-parser');
+const models = require('./models/models');
+
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+
+const User = models.User;
+const Thread = models.Thread;
+const Comment = models.Comment;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+
+passport.deserializeUser(function(id, done) {
+  // YOUR CODE HERE
+    User.findById(id)
+      .then(result => {
+          done(null, result);
+      })
+      .catch(err => {
+          console.log(err);
+      });
+});
+
+
+passport.use(new LocalStrategy(function(username, password, done) {
+  // Find the user with the given username
+    User.findOne({where: { username: username, password: password }}, function(err, user) {
+      // if there's an error, finish trying to authenticate (auth failed)
+        if (err) {
+            console.error('Error fetching user in LocalStrategy', err);
+            return done(err);
+        }
+      // if no user present, auth failed
+        if (!user) {
+            return done(null, false, { message: 'Incorrect username.' });
+        }
+      // if passwords do not match, auth failed
+        if (user.password !== password) {
+            return done(null, false, { message: 'Incorrect password.' });
+        }
+      // auth has has succeeded
+        return done(null, user);
+    });
+}
+));
 
 app.get('/', (request, response) => {
     response.sendFile(__dirname + '/public/index.html'); // For React/Redux
